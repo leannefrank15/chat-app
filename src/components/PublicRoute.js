9ef7e700c37439310bf793e068044b917208afa2
leannefrank <leannfrank@gmail.com>
2021-06-18 13:05:24 +0530

    created public and private routes

---
+++ src/components/PublicRoute.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Redirect, Route } from 'react-router';
+
+const PublicRoute = ({children, ...routeProps}) => {
+
+  const profile=false;
+
+  if(profile){
+    return (
+    <Redirect to="/">
+
+    </Redirect>
+    )
+  }
+
+  return (
+    <Route {...routeProps}>
+
+      {children}
+
+    </Route>
+  )
+}
+
+export default PublicRoute
