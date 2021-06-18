d63a29bec9d8a81a6267a5bad0d31147bf054ae5
leannefrank <leannfrank@gmail.com>
2021-06-18 20:55:43 +0530

    added sign in page and sign in with google

--- src/misc/firebase.js
+++ src/misc/firebase.js
@@ -1,4 +1,6 @@
 import firebase from 'firebase/app'
+import 'firebase/auth';
+import 'firebase/database'
 
 const config = {
   apiKey: "AIzaSyBKoRzMEzTwyzWQEspAUz8Sat7td1Nckdk",
@@ -10,3 +12,5 @@ const config = {
 };
 
 const app = firebase.initializeApp(config);
+export const auth = app.auth();
+export const database = app.database();
--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,10 +1,55 @@
 import React from 'react'
+import firebase from 'firebase/app'
+import { Container, Grid, Row, Panel, Button, Icon, Alert } from 'rsuite'
+import { Col } from 'rsuite'
+import { auth, database } from '../misc/firebase'
+import  '../styles/utility.scss'
 
-const SignIn = () => {
+const SignIn = () => { //grid has 24 columns. on mobiles, all 24 will be displayed
+
+  const signInWithProvider=async(provider)=>{
+    try{
+      const {additionalUserInfo, user} =await auth.signInWithPopup(provider)
+      Alert.success('signed in successfully', 4000)
+
+      if(additionalUserInfo.isNewUser){
+        await database.ref(`profiles/${user.uid}`).set({
+          name : user.displayName,
+          createdAt : firebase.database.ServerValue.TIMESTAMP
+        })
+      }
+    }
+    catch(err){
+      Alert.error(err.message, 4000); //milliseconds
+
+    }
+      
+  }
+
+  const onGoogleSignIn =()=>{
+    signInWithProvider(new firebase.auth.GoogleAuthProvider())
+  }
   return (
-    <div>
-      sign in page
-    </div>
+    <Container>
+      <Grid className="mt-page">
+        <Row>
+          <Col xs={24} md={12} mdOffset={6}> 
+            <Panel>
+             <div className='text-center'>
+               <h2>Welcome to chat</h2>
+               <p>progressive chat app for neophytes</p>
+             </div>
+
+             <div className="mt-3">
+               <Button block color="green" onClick={onGoogleSignIn}>
+                 <Icon icon="google"/> Continue with Google
+               </Button>
+             </div>
+            </Panel>
+          </Col>
+        </Row>
+      </Grid>
+    </Container>
   )
 }
 
