f1e19412bd8c474993cfd7f26d878ba82482f43a
leannefrank <leannfrank@gmail.com>
2021-06-22 15:04:13 +0530

    started building the sidebar component

---
+++ src/components/Sidebar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import DashboardToggle from './dashboard/DashboardToggle'
+
+const Sidebar = () => {
+  return (
+    <div className="h-100" pt-2>
+      
+      <div style={{marginTop:"10px"}}>
+        <DashboardToggle />
+      </div>
+
+      bottom
+
+    </div>
+  )
+}
+
+export default Sidebar
